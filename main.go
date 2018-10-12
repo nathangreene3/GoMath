b9e0a599ea31ac062678cf8a057b6a50d90ac248
nathangreene3 <nathangreene3@gmail.com>
2018-10-12 10:51:40 -0400

    Finished all pow functions.
    powInt and powFloat64 seem to work perfectly.  Need to add test cases to be certain.

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(powFloat64(2, 2.1))
+	fmt.Println(powFloat64(-2, 1.0/3.0))
+}
+
+// round returns x rounded to the nearest whole number as a float64.
+func round(x float64) float64 {
+	y := float64(int(x)) // Floors x
+	if 0.5 <= x-y {
+		y++
+	}
+	return y
 }
 
 // powInt returns x^n for any real x and any integer n.
@@ -20,8 +29,6 @@ func powInt(x float64, n int) float64 {
 		case n < 0:
 			y = 1 / powInt(x, -n)
 		case n == 0:
-			fallthrough
-		default:
 			y = 1
 		}
 	} else {
@@ -32,38 +39,29 @@ func powInt(x float64, n int) float64 {
 	return y
 }
 
-// powFloat64 returns x^y for any real x and y.
-//
+// powFloat64 returns x^y for real x and y. Some special cases cause panic.
 func powFloat64(x, y float64) float64 {
 	n := int(y)
 	r := y - float64(n)
-	m := int(1 / r)
+	m := int(round(1 / r))
 	v := powInt(x, n)
 	if r != 0 {
-		switch {
-		case 0 < x:
-			v *= newton(func(z float64) float64 { return powInt(z, m) - x }, 1, 0.000001)
-		case x < 0:
-			if m%2 != 0 {
-				v *= newton(func(z float64) float64 { return powInt(z, m) }, 1, 0.000001)
-			} else {
-				panic("indeterminant form")
-			}
-		case x == 0:
-			fallthrough
-		default:
-			v = 0
+		if x < 0 && m%2 == 0 {
+			panic("indeterminant form")
 		}
+		v *= newton(func(z float64) float64 { return powInt(z, m) - x }, 1, 0.000001) // Solves v^m - x = 0 for set x and m
 	}
 	return v
 }
 
+// sqrt returns x^0.5.
 func sqrt(x float64) float64 {
 	return powFloat64(x, 0.5)
 }
 
+// nthRoot returns x^(1/n).
 func nthRoot(x float64, n int) float64 {
-	return powFloat64(x, 1/float64(n))
+	return powFloat64(x, 1.0/float64(n))
 }
 
 // newton finds a local root assuming f is smooth and continuous. Note, this algorithm is known to be unstable.
@@ -76,10 +74,12 @@ func newton(f func(x float64) float64, x0, tol float64) float64 {
 	return x1
 }
 
+// diff returns the approximate value of df/dx at x0.
 func diff(f func(x float64) float64, x0, tol float64) float64 {
 	return (f(x0+tol) - f(x0-tol)) / (2 * tol)
 }
 
+// abs returns |x|.
 func abs(x float64) float64 {
 	if x < 0 {
 		x *= -1
